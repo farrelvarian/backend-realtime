e9af453cc7b5ac5cc2b9355091855586a44dad71
Farrel Varian Eka Putra <42968960+farrelvarian@users.noreply.github.com>
2021-09-22 15:25:29 +0700

    debug deploy

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ const usersModels = require("./src/models/users");
 // use middle
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-// const setupCors = {
-//   origin: `${process.env.FRONT_URL}`,
-// };
-app.use(cors());
+const optionCors = {
+  origin: [true, process.env.FRONT_URL],
+};
+app.use(cors(optionCors));
 
 app.use("/messages", messageRouter);
 app.use("/users", userRouter);
